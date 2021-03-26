0bb1b2773fdd307141fef660bb2d8139c469d756
Clarke <thatclarkemacarthur@gmail.com>
2021-03-26 19:47:21 -0400

    Learned Node and Express fundamentals, on to MongoDB! :rocket:

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,12 @@
 const app = require('./app');
+const dotenv = require('dotenv');
 
-const port = 3000;
+dotenv.config({ path: './config.env' });
+
+// NOTE: on Windows you have to enter SET NODE_ENV=development and then nodemon server.js - Jonas is running on Mac so  can do this all on one line and without the SET keyword.
+// console.log(process.env);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on ${port}...`);
 });
